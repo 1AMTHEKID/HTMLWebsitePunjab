d9076d3eea9a1891580061be62ac9cf7ce7d6964
1AMTHEKID <pascualfrankel@hotmail.com>
2019-05-15 14:30:50 +0200

    First commit.

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pascu
+ * Date: 15-5-2019
+ * Time: 11:39
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="includes/img/logo.png">
+    <title>Interstellar</title>
+</head>
+<body>
+<nav class="navigation"></nav>
+<div class="main-wrapper">
+    <div class="banner"></div>
+    <div class="leadtext"></div>
+    <div class="buttons">
+        <div class="button-left"></div>
+        <div class="button-middle"></div>
+        <div class="button-right"></div>
+    </div>
+    <div class="newsboxes">
+        <div class="newsbox-left"></div>
+        <div class="newbox-middle"></div>
+        <div class="newbox-right"></div>
+    </div>
+</div>
+<footer class="footer">
+    <div class="footer-left"></div>
+    <div class="footer-middle"></div>
+    <div class="footer-right"></div>
+</footer>
+</body>
+</html>
