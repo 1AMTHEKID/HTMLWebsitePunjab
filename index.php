c2841ae4b1933a9181076bb3d4c4a26489939cc0
1AMTHEKID <pascualfrankel@hotmail.com>
2019-05-16 14:10:53 +0200

    Navigation bar update.

--- index.php
+++ index.php
@@ -14,29 +14,64 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" type="image/png" href="includes/img/logo.png">
+    <link rel="shortcut icon" type="image/png" href="assets/img/logo.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
     <title>Interstellar</title>
+
 </head>
 <body>
-<nav class="navigation"></nav>
-<div class="main-wrapper">
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="navbar-collapse collapse justify-content-end w-100 order-1 order-md-0 dual-collapse2">
+            <a class="navbar-brand nav-margin mr-auto" href="#"><img class="nav-logo" src="assets/img/logo.png" alt=""></a>
+            <ul class="navbar-nav text-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="#button-ragnar"><h5>RAGNAR</h5></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#button-about"><h5>ABOUT</h5></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#button-contact"><h5>CONTACT</h5></a>
+                </li>
+            </ul>
+        </div>
+        <div class="mx-auto order-0">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
     <div class="banner"></div>
-    <div class="leadtext"></div>
-    <div class="buttons">
-        <div class="button-left"></div>
-        <div class="button-middle"></div>
-        <div class="button-right"></div>
-    </div>
-    <div class="newsboxes">
-        <div class="newsbox-left"></div>
-        <div class="newbox-middle"></div>
-        <div class="newbox-right"></div>
-    </div>
+    <div class="lead"></div>
+</header>
+<div class="container-fluid">
+    <section>
+        <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div id="button-ragnar" class="col-md-4"></div>
+            <div id="button-about" class="col-md-4"></div>
+            <div id="button-contact" class="col-md-4"></div>
+        </div>
+    </section>
 </div>
-<footer class="footer">
-    <div class="footer-left"></div>
-    <div class="footer-middle"></div>
-    <div class="footer-right"></div>
+<footer>
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+    </div>
 </footer>
+
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
